9335bc36b88023cc1e651d562772a7f1ef65f417
Nikola-jpg <84804348+Nikola-jpg@users.noreply.github.com>
2021-09-25 11:18:50 +0300

    added flyway and profile

--- src/main/java/com/example/sweater/entity/Message.java
+++ src/main/java/com/example/sweater/entity/Message.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
     private String text;
     private String tag;
@@ -46,7 +46,7 @@ public class Message {
         this.author = author;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
@@ -66,7 +66,7 @@ public class Message {
         this.text = text;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 }
