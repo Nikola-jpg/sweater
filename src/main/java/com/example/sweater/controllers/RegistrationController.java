5dd606c5c8fe1b21aa291b817b12ae708ade9322
Nikola-jpg <84804348+Nikola-jpg@users.noreply.github.com>
2021-10-04 13:56:28 +0300

    Add recaptcha (demo) and remember me

--- src/main/java/com/example/sweater/controllers/RegistrationController.java
+++ src/main/java/com/example/sweater/controllers/RegistrationController.java
@@ -1,19 +1,30 @@
 package com.example.sweater.controllers;
 
 import com.example.sweater.entity.User;
+import com.example.sweater.entity.dto.CaptchaResponseDto;
 import com.example.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
+import java.util.Collections;
 
 @Controller
 public class RegistrationController {
+    private final static String CAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response";
     @Autowired
     private UserService userService;
+
+    @Value("${recaptcha.secret}")
+    private String secret;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/registration")
     public String registration(@ModelAttribute("user") User user){
@@ -22,8 +33,20 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(@Valid User user, BindingResult bindingResult, Model model){
-        if(bindingResult.hasErrors()){
+    public String addUser(@RequestParam("g-recaptcha-response") String captchaResponse,
+                          @Valid User user,
+                          BindingResult bindingResult,
+                          Model model
+    ){
+        System.out.println(captchaResponse);
+
+        String url = String.format(CAPTCHA_URL, secret, captchaResponse);
+        CaptchaResponseDto response = restTemplate.postForObject(url, Collections.emptyList(), CaptchaResponseDto.class);
+
+        if(!response.isSuccess()){
+            model.addAttribute("captchaError", "Fill captcha");
+        }
+        if(bindingResult.hasErrors() || !response.isSuccess()){
             return "registration";
         }
 
